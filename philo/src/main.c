2ca87c3ab2e7be0afe73d2a1c0b6f016dbc8cd79
Markel Uribe Lasa <muribe-l@c3r7s1.42urduliz.com>
2024-06-05 14:27:22 +0200

    kaka

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: muribe-l <muribe-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 15:32:55 by muribe-l          #+#    #+#             */
-/*   Updated: 2024/06/04 19:14:59 by muribe-l         ###   ########.fr       */
+/*   Updated: 2024/06/05 14:26:27 by muribe-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,21 @@
 
 int	main(int argc, char **argv)
 {
-	t_philo			philos[PHILO_MAX];
-	t_program		program;
+	t_philo			philos;
 	pthread_mutex_t	forks[PHILO_MAX];
+	t_program		program;
 
+	philos = NULL;
+
+	initialize_philos(&philos);
+	if (*philos == NULL)
+		*philos = new_node;
+	else
+		ft_lst_addback(new_node);
 	if (!parse(argc, argv, &program))
 		return (1);
-	
+	program.philos = philos;
+	init_forks(forks, argv[1]);
 	return (0);
 }
 

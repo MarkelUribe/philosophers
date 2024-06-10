5c518cea95f6d2480108888c64324ce7d1d8d672
Markel Uribe Lasa <uribemarkel@gmail.com>
2024-06-10 19:20:03 +0200

    gauzak

--- philo/src/destroy.c
+++ philo/src/destroy.c
@@ -6,17 +6,19 @@
 /*   By: muribe-l <muribe-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 16:35:24 by muribe-l          #+#    #+#             */
-/*   Updated: 2024/06/05 16:59:14 by muribe-l         ###   ########.fr       */
+/*   Updated: 2024/06/10 18:41:19 by muribe-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-void	destroy_all(t_philo *philos, t_data *data)
+/* Destroy all the elements and stop the program */
+void	destroy_all(char *error, t_philo *philos, t_data *data)
 {
 	int i;
 	
 	i = 0;
+	write(2, error, ft_strlen(error));
 	while (i < data->n_philo)
 	{
 		if (&philos[i].fork)

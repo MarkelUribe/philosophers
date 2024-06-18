c09221849ec6e128d48a7fdb96a4ed6416fe4a2f
MarkelUribe <uribemarkel@gmail.com>
2024-06-18 17:09:27 +0200

    broken

--- philo/src/monitor.c
+++ philo/src/monitor.c
@@ -6,7 +6,7 @@
 /*   By: markel <markel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 14:28:25 by muribe-l          #+#    #+#             */
-/*   Updated: 2024/06/17 15:31:50 by markel           ###   ########.fr       */
+/*   Updated: 2024/06/18 16:53:51 by markel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 /* Cheks if dinner ended so that the program finishes */
 int	dinner_ended(t_data *data)
 {
+	if (data == NULL)
+		return (1);
 	pthread_mutex_lock(&data->table_mutex);
 	if (data->dinner_ended)
 		return (pthread_mutex_unlock(&data->table_mutex), 1);
@@ -42,6 +44,8 @@ int	everyone_ate(t_philo **philos)
 	int	i;
 	int	meals;
 
+	if ((*philos)->data == NULL)
+		return (1);
 	if ((int)(*philos)->data->times_philo_must_eat == -1)
 		return (0);
 	i = 0;
@@ -66,7 +70,7 @@ void	iter_philos(t_philo **p)
 {
 	int	i;
 	
-	while (!dinner_ended((*p)[0].data))
+	while (!dinner_ended((*p)[0].data) && !everyone_ate(p))
 	{
 		i = -1;
 		while (++i < (*p)[0].data->n_philo)
@@ -77,6 +81,7 @@ void	iter_philos(t_philo **p)
 				printf("Dinner finished\n");
 				(*p)[0].data->dinner_ended = 1;
 				pthread_mutex_unlock(&(*p)->data->table_mutex);
+				return ;
 			}
 		}
 	}

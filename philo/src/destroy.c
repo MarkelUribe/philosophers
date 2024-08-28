e5d84fa00b4edefd24a55477bb8677f791c3621b
MarkelUribe <uribemarkel@gmail.com>
2024-08-28 14:04:44 +0200

    destroy fixed

--- philo/src/destroy.c
+++ philo/src/destroy.c
@@ -6,11 +6,18 @@
 /*   By: muribe-l <muribe-l@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 16:35:24 by muribe-l          #+#    #+#             */
-/*   Updated: 2024/08/13 16:14:05 by muribe-l         ###   ########.fr       */
+/*   Updated: 2024/08/28 13:29:27 by muribe-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
+
+/* Checks if mutex exists and destroys it */
+static void	destroy_mutex(pthread_mutex_t *mutex)
+{
+	if (mutex)
+		pthread_mutex_destroy(mutex);
+}
 
 /* Destroy all the elements and stops the program */
 void	destroy_all(char *error, t_philo *philos, t_data *data)
@@ -22,23 +29,15 @@ void	destroy_all(char *error, t_philo *philos, t_data *data)
 		printf(ADMIN"Error: %s\n"RESET, error);
 	while (++i < data->n_philo)
 	{
-		if (&data->forks[i])
-			pthread_mutex_destroy(&data->forks[i]);
-		if (&philos[i].meal_mutex)
-			pthread_mutex_destroy(&philos[i].meal_mutex);
+		destroy_mutex(&data->forks[i]);
+		destroy_mutex(&philos[i].meal_mutex);
 	}
-	if (&data->print_mutex)
-		pthread_mutex_destroy(&data->print_mutex);
-	if (&data->table_mutex)
-		pthread_mutex_destroy(&data->table_mutex);
-	if (&data->death_mutex)
-		pthread_mutex_destroy(&data->death_mutex);
-	if (data->forks != NULL)
-		free(data->forks);
-	if (philos != NULL)
-		free(philos);
-	if (data)
-		free(data);
+	destroy_mutex(&data->print_mutex);
+	destroy_mutex(&data->table_mutex);
+	destroy_mutex(&data->death_mutex);
+	free(data->forks);
+	free(philos);
+	free(data);
 	exit(1);
 }
 
@@ -47,22 +46,14 @@ void	destroy_data(char *error, t_data *data)
 {
 	int	i;
 
-	i = 0;
+	i = -1;
 	if (ft_strlen(error) != 0)
 		printf(ADMIN"Error: %s\n"RESET, error);
-	while (i < data->n_philo)
-	{
-		if (&data->forks[i])
-			pthread_mutex_destroy(&data->forks[i]);
-		i++;
-	}
-	if (&data->print_mutex)
-		pthread_mutex_destroy(&data->print_mutex);
-	if (&data->table_mutex)
-		pthread_mutex_destroy(&data->table_mutex);
-	if (&data->death_mutex)
-		pthread_mutex_destroy(&data->death_mutex);
-	if (data->forks != NULL)
-		free(data->forks);
+	while (++i < data->n_philo)
+		destroy_mutex(&data->forks[i]);
+	destroy_mutex(&data->print_mutex);
+	destroy_mutex(&data->table_mutex);
+	destroy_mutex(&data->death_mutex);
+	free(data->forks);
 	exit(1);
 }
